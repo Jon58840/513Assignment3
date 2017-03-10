d4feaad6bd0fb455dcea242ef84f291a5596a9d4
Jon58840 <jon58840@hotmail.com>
2017-03-10 02:40:04 -0700

    Stuff and things

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-http.listen( port, function () {
-    console.log('listening on port', port);
-});
-
 app.use(express.static(__dirname + '/public'));
 
 // listen to 'chat' messages
 io.on('connection', function(socket){
-    socket.on('chat', function(msg){
-	io.emit('chat', msg);
-    });
+  console.log('A user has connected.');
+  
+  socket.on('disconnect', function(msg){
+    console.log('A user has disconnected.');
+  });
+  
+  socket.on('chat', function(msg){
+	console.log('Message: ' + msg);
+    io.emit('chat', msg);
+  });
+});
+
+http.listen( port, function () {
+    console.log('listening on port', port);
 });
--- public/client.js
+++ public/client.js
@@ -1,12 +1,14 @@
 // shorthand for $(document).ready(...)
 $(function() {
     var socket = io();
-    $('form').submit(function(){
-	socket.emit('chat', $('#m').val());
-	$('#m').val('');
-	return false;
+	
+    $('form').submit(function() {
+	   socket.emit('chat', $('#m').val());
+	  $('#m').val('');
+	  return false;
     });
+	
     socket.on('chat', function(msg){
-	$('#messages').append($('<li>').text(msg));
+	  $('#messages').append($('<li>').text(msg));
     });
 });
